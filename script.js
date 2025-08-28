4fb5a353f82894ef74b8651290be46fb1ec6af49
Deep Prajapati <dvprajapati7212@gmail.com>
2025-08-28 18:46:06 +0530

    feat: implement localstorage to store task in browser storage

--- script.js
+++ script.js
@@ -15,16 +15,33 @@ addBtn.addEventListener("click", () => {
         let delBtn = document.createElement("button");
         delBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
         li.appendChild(delBtn);
+        storeToStorage();
     }
     ip.value = '';
 });
 
 listContainer.addEventListener("click", (e) => {
     if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked")
+        e.target.classList.toggle("checked");
+        storeToStorage();
     } else if (e.target.tagName === "BUTTON" || e.target.parentElement.tagName==="BUTTON") {
         const li = e.target.closest("li");
         li.remove();
+        storeToStorage();
     }
     
 })
+
+// Store into Local Storage
+
+function storeToStorage() {
+    localStorage.setItem("task", listContainer.innerHTML);
+}
+
+// Display Task from local storage
+
+function displayTask() {
+    listContainer.innerHTML = localStorage.getItem("task");
+}
+
+displayTask();
